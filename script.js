1d557e1e61a689673725dd9ed317716ab7409c38
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-07-19 15:11:48 +0530

    Calculator JavaScript

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-const item = document.getElementById("item");
-const toD0Box = document.getElementById("to_D0_Box");
-
-item.addEventListener("keyup", function (event) {
-  if (event.key == "Enter") {
-    addToDo(this.value);
-    this.value = "";
-  }
-});
-
-const addToDo = (itemText) => {
-  const listItem = document.createElement("li");
-  toD0Box.appendChild(listItem);
-  listItem.innerHTML = `
-    ${itemText} <i class="fas fa-times" aria-hidden="true"></i>
-  `;
-
-  listItem.addEventListener("click", function () {
-    this.classList.toggle("done");
-    const crossIcon = this.querySelector("i");
-    crossIcon.classList.toggle("red-cross");
-  });
-};
-
-toD0Box.addEventListener("click", function (event) {
-  if (
-    event.target &&
-    event.target.nodeName == "I" &&
-    event.target.classList.contains("fa-times")
-  ) {
-    event.target.parentNode.remove();
-  }
-});
+function calculator ()
+{
+    var val_1 = document.getElementById('n1').value;
+    var opr_1 = document.getElementById('opr').value;
+    var val_2  = document.getElementById('n2').value;
+    
+    if(opr_1 == '+')
+    {
+     var result = parseInt(val_1) + parseInt(val_2);
+    }
+    if(opr_1 == '-')
+    {
+     var result = parseInt(val_1) - parseInt(val_2);
+    }
+    if(opr_1 == 'X')
+    {
+     var result = parseInt(val_1) * parseInt(val_2);
+    }
+    if(opr_1 == '/')
+    {
+     var result = parseInt(val_1) / parseInt(val_2);
+    }
+    
+    document.getElementById('result').value = result ;
+      alert(Calculate, Done);
+}
